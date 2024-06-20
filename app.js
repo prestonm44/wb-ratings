dcd05270038aa6ed55dfa2a93fb8c811d0713455
prestonm44 <preston.mitchell4444@gmail.com>
2024-06-19 21:56:14 -0600

    assignment portion complete

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+import { Movie, Rating, User } from './src/model.js'
 import express from 'express';
 import session from 'express-session';
 import morgan from 'morgan';
@@ -13,3 +15,14 @@ app.use(express.json());
 app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: false }));
 
 ViteExpress.listen(app, port, () => console.log(`Server is listening on http://localhost:${port}`));
+
+app.get('/api/movies', async (req,res) => {
+    const allMovies = await Movie.findAll();
+    res.json(allMovies)
+});
+
+app.get('/api/movies/:movieId', async (req,res) => {
+    const { movieId } = req.params;
+    const movie = await Movie.findByPk(movieId);
+    res.json(movie);
+});
